e9c5a90cd85c52c811dba5aeb78072024a1786fb
Rimurutempest187 <karangmerry@gmail.com>
2026-02-22 18:38:32 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+BOT_TOKEN = "8442181435:AAHigN8UCaH3hZ5_5Jkw25AnGXM7uKoxNik"
+# Admin user IDs (list of integers)
+ADMINS = [1812962224]  # replace with real admin Telegram user IDs
+# Default timezone for scheduling
+TIMEZONE = "Asia/Yangon"
+# Footer text appended to user-facing lists/messages
+FOOTER = "\n\nCreate by : PINLON-YOUTH"
